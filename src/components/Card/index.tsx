6176918266922872ec98504ce98c2725d10a9ad6
SergioBSP <sergiodominguezgarcia993@gmail.com>
2023-05-30 07:10:22 +0200

    feat: implement localStorage

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,12 +1,28 @@
-import {FC} from "react";
+import { FC } from 'react'
+import "./styles.css"
 
 
-const Card : FC =  () => {
-   return (
-      <div>
-         
+
+const Card: FC = () => {
+  return (
+
+  <div className='cardContainer'>
+    <div className='cardContent'>
+
+
+      <div className='cardHeader'>
+      TITULO
       </div>
-   )
+
+      <div className='cardFooter'>
+      IMAGEN
+      </div>
+
+
+    </div>
+  </div>
+
+  )
 }
 
 export default Card
---
+++ src/services/storage/Photos.ts
@@ -0,0 +1,13 @@
+import { Photo } from "../../models/Photo"
+
+const NASA_PHOTO_ID = 'nasaPhotos'
+
+
+export const getCachedNasaPhotos = (): Photo[] => {
+  const response = window.localStorage.getItem(NASA_PHOTO_ID)
+  return response ? JSON.parse(response) : []
+}
+
+export const setCachedNasaPhotos = (photos: Photo[]) => {
+  window.localStorage.setItem(NASA_PHOTO_ID, JSON.stringify(photos))
+}
