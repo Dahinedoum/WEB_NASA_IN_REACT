87f405db2b12f567f93e3e2f7cb4763fa58e4e65
NihadMouden <nihadmouden@gmail.com>
2023-06-02 21:01:56 +0200

    Browser

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,23 @@
-import { FC } from 'react'
-import "./styles.css"
-
+import { FC, useCallback } from 'react'
+import './styles.css'
+import Button from '@mui/material/Button'
+import { useNavigate } from 'react-router-dom'
 
 const Header: FC = () => {
+  const navigate = useNavigate()
+
+  const handleButtonClick = useCallback(() => {
+    navigate('/login')
+  }, [navigate])
   return (
     <header className="headerContainer">
-      <div className='headerContent'>
-        <h2>Exploring Mars</h2>
+      <div className="headerContent">
+        <img src="../logo.png" alt="Logo" className="logo" />
+      </div>
+      <div className="buttonContainer">
+        <Button className="button" variant="text" onClick={handleButtonClick}>
+          Rumbo hacía Marte
+        </Button>
       </div>
     </header>
   )
