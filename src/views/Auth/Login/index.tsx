f9c6bfc3f3341d23206d67dec6920ed16a38f4fc
NihadMouden <nihadmouden@gmail.com>
2023-06-02 14:55:20 +0200

    To NOT merge

--- src/views/Auth/Login/index.tsx
+++ src/views/Auth/Login/index.tsx
@@ -1,7 +1,23 @@
 import { FC, memo } from 'react'
+import { Container, Form, Input, InputController, Label } from './styles'
 
 const Login: FC = () => {
-  return <div>LOGIN VIEW</div>
+  return (<Container>
+    <Form>
+        <InputController>
+            <Label>Email</Label>
+            <Input type="email" />
+        </InputController>
+        <InputController>
+            <Label>Password</Label>
+            <Input type="password" />
+        </InputController>
+        <Button></Button>
+    </Form>
+
+    
+  </Container>
+  )
 }
 
 export default memo(Login)
