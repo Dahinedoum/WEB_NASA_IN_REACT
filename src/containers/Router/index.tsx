bfde63ecfa76123710721745fec218d4966e10d9
sergioBSP <sergiodominguezgarcia993@gmail.com>
2023-06-02 19:08:56 +0200

    feat: addFav finished

--- src/containers/Router/index.tsx
+++ src/containers/Router/index.tsx
@@ -1,20 +1,20 @@
 import React, { FC } from 'react'
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+// import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Dashboard from '../../views/Dashboard'
 import Login from '../../views/Auth/Login'
 import Signup from '../../views/Auth/Signup'
 
 const Router: FC = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/*" element={<Navigate replace to="/dashboard" />} />
-      </Routes>
-    </BrowserRouter>
-  )
+  //   <BrowserRouter>
+  //     <Routes>
+  //       <Route path="/dashboard" element={<Dashboard />} />
+  //       <Route path="/login" element={<Login />} />
+  //       <Route path="/signup" element={<Signup />} />
+  //       <Route path="/*" element={<Navigate replace to="/dashboard" />} />
+  //     </Routes>
+  //   </BrowserRouter>
+  // )
 }
 
 export default Router
