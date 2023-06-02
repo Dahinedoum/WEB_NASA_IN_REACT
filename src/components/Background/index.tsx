f5e008759638df03fe8cc14a797744dfe867b439
NihadMouden <nihadmouden@gmail.com>
2023-06-02 20:24:42 +0200

    Browser

---
+++ src/components/Background/index.tsx
@@ -0,0 +1,14 @@
+import { FC } from 'react'
+import './styles.css'
+
+const Background: FC = () => {
+  return (
+    <div className="video-container">
+      <video autoPlay muted loop className="video-background">
+        <source src="../Background.mp4" type="video/mp4" />
+      </video>
+    </div>
+  )
+}
+
+export default Background
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+import { FC } from 'react'
+import './styles.css'
+
+const Footer: FC = () => {
+  return (
+    <footer className="footerContainer">
+      <div className="footerContent">
+        <h2 className="footerText">© 2023 Exploring Mars by NASA</h2>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
