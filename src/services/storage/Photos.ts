a1232ded87e98dfc01248aeb888131d614ddd6f4
sergioBSP <sergiodominguezgarcia993@gmail.com>
2023-06-03 10:30:33 +0200

    feat: remove card

--- src/services/storage/Photos.ts
+++ src/services/storage/Photos.ts
@@ -1,9 +1,7 @@
-
-import { Photo } from "../../models/Photo"
+import { Photo } from '../../models/Photo'
 
 const NASA_PHOTO_ID = 'nasaPhotos'
 export const FAV_PHOTOS = 'favPhotos'
-
 
 export const getCachedNasaPhotos = (): Photo[] => {
   const response = window.localStorage.getItem(NASA_PHOTO_ID)
@@ -16,6 +14,40 @@ export const setCachedNasaPhotos = (photos: Photo[]) => {
 
 
 
+
+
+
+export const addCachedNasaPhotos = (photos: Photo) => {
+  const currentCachedPhotos = getCachedNasaPhotos()
+  const existingIndex = currentCachedPhotos.findIndex(
+    (item) => item.id === photos.id
+  )
+  if (existingIndex !== -1) {
+    currentCachedPhotos.splice(existingIndex, 1)
+  } else {
+    currentCachedPhotos.push(photos)
+  }
+  setCachedNasaPhotos(currentCachedPhotos)
+}
+
+export const removeCachedNasaPhotos = (photos: Photo) => {
+  const currentCachedPhotos = getCachedNasaPhotos()
+  const existingIndex = currentCachedPhotos.findIndex(
+    (item) => item.id === photos.id
+  )
+  if (existingIndex !== -1) {
+    currentCachedPhotos.splice(existingIndex, 1)
+  } 
+  setCachedNasaPhotos(currentCachedPhotos)
+  return currentCachedPhotos
+}
+
+
+
+
+
+
+
 export const getFavPhotos = (): Photo[] => {
   const response = window.localStorage.getItem(FAV_PHOTOS)
   return response ? JSON.parse(response) : []
@@ -25,34 +57,15 @@ export const setFavPhotos = (photos: Photo[]) => {
   window.localStorage.setItem(FAV_PHOTOS, JSON.stringify(photos))
 }
 
-// export const addFavPhoto = (photo: Photo) => {
-//   const currentFavPhotos = getFavPhotos()
-//   const updatedFavPhotos = [...currentFavPhotos, photo]
-//   setFavPhotos(updatedFavPhotos)
-// }
-
-
-
 export const addFavPhoto = (photo: Photo) => {
   const currentFavPhotos = getFavPhotos()
-
-  // Verificar si el elemento ya está en la lista de favoritos
-  const existingIndex = currentFavPhotos.findIndex((item) => item.id === photo.id)
-
+  const existingIndex = currentFavPhotos.findIndex(
+    (item) => item.id === photo.id
+  )
   if (existingIndex !== -1) {
-    // Si el elemento ya está en favoritos, eliminarlo
     currentFavPhotos.splice(existingIndex, 1)
   } else {
-    // Si el elemento no está en favoritos, agregarlo
     currentFavPhotos.push(photo)
   }
-
   setFavPhotos(currentFavPhotos)
 }
-
-
-
-
-
-
-
