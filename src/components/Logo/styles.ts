b00063c0869017c1ccb94b01a5618871b797867d
NihadMouden <nihadmouden@gmail.com>
2023-06-09 02:40:35 +0200

    refactor: Buttons

--- src/components/Logo/styles.ts
+++ src/components/Logo/styles.ts
@@ -1,14 +1,15 @@
 import styled from 'styled-components';
 
 const Logo = styled.img`
-   height: 100px; /* Aumenta la altura del logo */
-   width: auto; /* Deja que el ancho se ajuste proporcionalmente */
+   height: 100px; 
+   width: auto; 
    position: fixed;
-   top: 20px; /* Ajusta la posición vertical */
-   left: 20px; /* Ajusta la posición horizontal */
-   border-radius: 50%; /* Aplica un borde redondeado */
-   border: 2px solid #000; /* Añade un borde al logo */
-   box-shadow: 0 0 10px rgba(0, 0, 0, 0.2); /* Agrega una sombra */
+   top: 20px; 
+   left: 20px; 
+   border-radius: 50%; 
+   border: 1px solid #000; 
+   box-shadow: 0 0 10px rgba(0, 0, 0, 0.2); 
+   background-color: rgba(128, 128, 100, 0.4);
 `;
 
 export { Logo };
